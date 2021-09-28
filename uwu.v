6bd2f4b9eb758bd180085e637a810b72bb56f02c
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-09-28 19:18:16 +0000

    uwu: add function to ensure the presence of an executable

--- uwu.v
+++ uwu.v
@@ -29,6 +29,14 @@ pub fn need_env(name string) ?string {
 	return res
 }
 
+// need_exe ensures the presence of the requested executable.
+// return the absolute path to the executable or terminate the program execution.
+pub fn need_exe(name string) string {
+	return os.find_abs_path_of_executable(name) or {
+		catch(ups.item_not_found_error('executable', name))
+	}
+}
+
 // catch will print the error message and exit with the error code.
 [noreturn]
 pub fn catch(err IError) {
