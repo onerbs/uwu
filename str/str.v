f5a7a3e7aaa2335ec819c5d3541c7da1d3e8815c
AAAA <dev@onerbs.com>
2022-12-07 21:54:27 -0600

    misc: str: update code style

--- str/str.v
+++ str/str.v
@@ -5,61 +5,61 @@ import uwu.buffer
 // brace will surround the base string in a brace pair or
 // place a bracer at the end and another at the beginning
 // of the base string.
-pub fn brace(base string, bracer string) string {
+pub fn brace(s string, bracer string) string {
 	pair := str.braces_s[bracer] or { bracer }
-	return '$bracer$base$pair'
+	return '${bracer}${s}${pair}'
 }
 
 // brace will surround the base string into.
-pub fn brace_mirror(base string, bracer string) string {
+pub fn brace_mirror(s string, bracer string) string {
 	pair := mirror(bracer)
-	return '$bracer$base$pair'
+	return '${bracer}${s}${pair}'
 }
 
 // digits extract the numeric characters from a string.
-pub fn digits(base string) string {
-	mut buf := []byte{cap: base.len}
-	for c in base {
+pub fn digits(s string) string {
+	mut buf := buffer.cap(s.len)
+	for c in s {
 		if c.is_digit() {
 			buf << c
 		}
 	}
-	return buf.bytestr()
+	return buf.str()
 }
 
 // grow append the necessary spaces to the end of the provided
 // string until the length match the specified size.
-pub fn grow(base string, size int) string {
-	if size > base.len {
-		unsafe {
-			gap := size - base.len
-			buf := malloc(size)
-			vmemcpy(buf, base.str, base.len)
-			vmemset(buf + base.len, 0x20, gap)
-			return buf.vstring_with_len(size)
-		}
+pub fn grow(s string, size int) string {
+	if size <= s.len {
+		return s
 	}
-	return base
+	gap := size - s.len
+	unsafe {
+		buf := &u8(malloc(size))
+		vmemcpy(buf, s.str, s.len)
+		vmemset(buf + s.len, 0x20, gap)
+		return buf.vstring_with_len(size)
+	}
 }
 
 // grow_left append the necessary spaces to the beginning of the
 // provided string until the length match the specified size.
-pub fn grow_left(base string, size int) string {
-	if size > base.len {
-		unsafe {
-			gap := size - base.len
-			buf := malloc(size)
-			vmemset(buf, 0x20, gap)
-			vmemcpy(buf + gap, base.str, base.len)
-			return buf.vstring_with_len(size)
-		}
+pub fn grow_left(s string, size int) string {
+	if size <= s.len {
+		return s
 	}
-	return base
+	gap := size - s.len
+	unsafe {
+		buf := &u8(malloc(size))
+		vmemset(buf, 0x20, gap)
+		vmemcpy(buf + gap, s.str, s.len)
+		return buf.vstring_with_len(size)
+	}
 }
 
 // key_value extract a key-value pair from a string.
-pub fn key_value(base string, delim string) (string, string) {
-	pts := base.split_nth(delim, 2)
+pub fn key_value(s string, delimiter string) (string, string) {
+	pts := s.split_nth(delimiter, 2)
 	key := pts[0] or { '' }
 	val := pts[1] or { '' }
 	return key, val
@@ -67,10 +67,10 @@ pub fn key_value(base string, delim string) (string, string) {
 
 // lines extract the lines from a string, trimming white spaces
 // and skipping empty lines.
-pub fn lines(base string) []string {
+pub fn lines(s string) []string {
+	mut buf := buffer.cap(s.len)
 	mut res := []string{}
-	mut buf := buffer.cap(base.len)
-	for c in base {
+	for c in s {
 		if c == `\n` {
 			lin := buf.strip()
 			if lin.len > 0 {
@@ -90,17 +90,17 @@ pub fn lines(base string) []string {
 // mirror will reverse the base string, also mirroring any
 // supported character.
 [direct_array_access]
-pub fn mirror(base string) string {
-	mut buf := []byte{cap: base.len}
-	for i := base.len - 1; i >= 0; i-- {
-		c := base[i]
+pub fn mirror(s string) string {
+	mut buf := buffer.cap(s.len)
+	for i := s.len - 1; i >= 0; i-- {
+		c := s[i]
 		if c in str.braces_b {
 			buf << str.braces_b[c]
 		} else {
 			buf << c
 		}
 	}
-	return buf.bytestr()
+	return buf.str()
 }
 
 // quote will surround the provided string in double quotes.
@@ -133,57 +133,60 @@ pub fn safe_single_quote(base string) string {
 
 // repeat create a string with len n only containing the
 // character c.
-pub fn repeat(c byte, n int) string {
-	if _unlikely_(n < 1 || c < 1) {
+pub fn repeat(c u8, n int) string {
+	if c < 1 || n < 1 {
 		return ''
 	}
 	unsafe {
-		buf := &byte(vmemset(malloc(n), c, n))
+		buf := &u8(malloc(n + 1))
+		buf[n] = 0
+		assert !isnil(vmemset(buf, c, n))
 		return buf.vstring_with_len(n)
 	}
 }
 
 // repeat_string will create a new string with n repetitions
 // of the base string.
-pub fn repeat_str(base string, n int) string {
-	if n < 1 || base.len == 0 {
-		return ''
-	}
-	len := base.len * n
-	buf := unsafe { malloc(len) }
-	mut ptr := buf
-	for _ in 0 .. n {
-		unsafe {
-			vmemcpy(ptr, base.str, base.len)
-			ptr += base.len
+pub fn repeat_str(s string, n int) string {
+	len := s.len * n
+	mut ofs := 0
+	unsafe {
+		buf := &u8(malloc(len + 1))
+		for _ in 0 .. n {
+			assert !isnil(vmemcpy(buf + ofs, s.str, s.len))
+			ofs += s.len
 		}
+		buf[len] = 0
+		return buf.vstring_with_len(len)
 	}
-	return unsafe { buf.vstring_with_len(len) }
 }
 
 // space return the base string with one space between every character.
-pub fn space(base string) string {
-	if base.len < 1 {
+pub fn space(s string) string {
+	if s.len < 1 {
 		return ''
 	}
-	len := base.len * 2
+	len := (s.len * 2) - 1
 	unsafe {
-		mut buf := malloc(len)
-		for i in 0 .. base.len {
-			ix := i * 2
-			buf[ix] = base[i]
-			buf[ix + 1] = ` `
+		mut buf := &u8(malloc(len + 1))
+		mut i := 0
+		for c in s {
+			buf[i] = c
+			i++
+			buf[i] = ` `
+			i++
 		}
-		return buf.vstring_with_len(len - 1)
+		buf[len] = 0
+		return buf.vstring_with_len(len)
 	}
 }
 
 // words extract the words from a string.
 // i.e. find all matches of the regular expression '\S+'.
-pub fn words(base string) []string {
+pub fn words(s string) []string {
+	mut buf := buffer.cap(s.len)
 	mut res := []string{}
-	mut buf := buffer.cap(base.len)
-	for c in base {
+	for c in s {
 		if c.is_space() {
 			if buf.len > 0 {
 				res << buf.str()
