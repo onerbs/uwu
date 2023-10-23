94dc73a8384a4e754ec08cdfc8a7bba8764265a1
AAAA <dev@onerbs.com>
2023-10-23 08:32:09 -0600

    paths: add `chext` function

--- paths/paths.v
+++ paths/paths.v
@@ -3,16 +3,32 @@ module paths
 import os
 
 // from_here build a path from the current directory.
+[inline]
 pub fn from_here(dirs ...string) string {
 	return os.join_path(os.getwd(), ...dirs)
 }
 
 // from_home build a path from the user home directory.
+[inline]
 pub fn from_home(dirs ...string) string {
 	return os.join_path(os.home_dir(), ...dirs)
 }
 
 // simple return the basename of the file without extension.
 pub fn simple(base string) string {
-	return os.base(base).all_before('.')
+	mut res := os.base(base)
+	if res.contains('.') {
+		res = res.all_before_last('.')
+	}
+	return res
+}
+
+// chext change the filename extension.
+pub fn chext(base string, next string) string {
+	mut res := base
+	if res.contains('.') {
+		res = res.all_before_last('.')
+	}
+	ext := next.trim('.')
+	return if ext.len > 0 { '${res}.${ext}' } else { res }
 }
---
+++ paths/paths_test.v
@@ -0,0 +1,12 @@
+import uwu.paths
+
+fn test_simple() {
+	assert 'uwu.c' == paths.simple('some/uwu.c.v')
+	assert 'uwu' == paths.simple('some/uwu.v')
+}
+
+fn test_chext() {
+	assert 'some/uwu.c' == paths.chext('some/uwu.v', '.c')
+	assert 'some/uwu.c' == paths.chext('some/uwu', 'c')
+	assert 'uwu.c' == paths.chext('uwu', 'c')
+}
