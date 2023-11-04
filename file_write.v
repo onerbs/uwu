1c96ce00ea461a4bca2a3ea725a6ed449ad748fd
alex ヾ(•ω•`)o <dev@onerbs.com>
2023-11-04 03:29:10 -0600

    uwu: use `fwrite` and `fputs` instead of `fprintf` to write into files

--- file_write.v
+++ file_write.v
@@ -2,18 +2,18 @@ module uwu
 
 // write_bytes will read and return a buffer with all bytes in the source file.
 [inline]
-pub fn (self File) write_bytes(dat []u8) {
-	C.fprintf(self.ref, &char(dat.data))
+pub fn (f File) write_bytes(dat []u8) {
+	C.fwrite(dat.data, 1, dat.len, f.ref)
 }
 
 // write_text will read and return a string with the content of the source file.
 [inline]
-pub fn (self File) write_text(msg string) {
-	self.write_bytes(msg.bytes())
+pub fn (f File) write_text(msg string) {
+	C.fputs(msg.str, f.ref)
 }
 
 // write_lines read all lines from the source file.
 [inline]
-pub fn (self File) write_lines(lines ...string) {
-	self.write_text(lines.join('\n'))
+pub fn (f File) write_lines(lines ...string) {
+	f.write_text(lines.join('\n'))
 }
--- test/file_test.v
+++ test/file_test.v
@@ -1,23 +1,37 @@
 import uwu
 
 fn test_read_bytes() {
-	mut rft := uwu.open('read_file_test.txt', 'r')!
+	mut rft := uwu.open('test_read_file.txt', 'r')!
+	defer {
+		rft.close()
+	}
 
 	assert rft.read_bytes() == [u8(`a`), `\n`, `b`, `\n`, `c`, `\n`, `d`, `\n`, `\n`, `0`, `\n`,
 		`1`, `\n`, `2`, `\n`, `3`, `\n`]
 	assert rft.read_text() == 'a\nb\nc\nd\n\n0\n1\n2\n3\n'
 	assert rft.read_lines().len == 10
-	rft.close()
 }
 
 fn test_write_bytes() {
-	mut wft := uwu.open('write_file_test.txt', 'w')!
+	mut wft := uwu.open('test_write_bytes.txt', 'w')!
 
 	dat := [u8(`A`), `h`, `o`, `y`, `!`, `\n`]
 	wft.write_bytes(dat)
 	wft.close()
 
-	wft = uwu.open('write_file_test.txt', 'r')!
+	wft = uwu.open('test_write_bytes.txt', 'r')!
 	assert dat == wft.read_bytes()
+	wft.close()
+}
+
+fn test_write_text() {
+	mut wft := uwu.open('test_write_text.txt', 'w')!
+
+	dat := 'this is a sample text'
+	wft.write_text(dat)
+	wft.close()
+
+	wft = uwu.open('test_write_text.txt', 'r')!
+	assert dat == wft.read_text()
 	wft.close()
 }
